25bfdf32d4474921ecd748fe1e4226876e003920
jabellorin05 <jabellorin@gmail.com>
2024-09-16 16:24:03 -0400

    fix bug when there is not data in the list

--- src/components/SearchMovie.vue
+++ src/components/SearchMovie.vue
@@ -6,9 +6,14 @@
     <button v-on:click="fetchData">Search</button>
 
     
-   
-
-    <ShowData :movieName="movieList"/>
+   <!-- conditionally render the ShowData component only if the movieList array has at least one item. -->
+    <div v-if="movieList.length > 0">
+        <ShowData :movieName="movieList"/>
+      
+    </div>
+    <div v-else>
+        <p>No movie found</p>
+    </div>
   
 </template>
 <script>
@@ -22,38 +27,45 @@ export default {
         return{
           
             searchName:"",
-            movieList: [],   
+            movieList: [],  
+            isLoading: true 
         }
     },
     
     methods:{
         fetchData(){
-            var apikey = "d3e5991c";
-               if (this.searchName.trim() === '') {
-                alert('Please enter a movie name');
-                return; 
-            }
            
+           var apikey = "d3e5991c";
+
+          if (this.searchName.trim() === '') {
+           alert('Please enter a movie name');
+           return; 
+       }
+      
+   
+    var url = `http://www.omdbapi.com/?apikey=${apikey}&s=${this.searchName}`;
+
+       fetch(url).then(response=>{
+           if(!response.ok){
+               throw Error('ERROR');
+           }
+           return response.json();
+       }).then(data=>{
+           //console.log(data);
+           this.movieList = data.Search;
+           this.isLoading = false;
+
+       }).catch(error=>{
+           console.log('error getting the data with the api'+error);
+       })
+
+   }
+      
         
-         var url = `http://www.omdbapi.com/?apikey=${apikey}&s=${this.searchName}`;
-
-            fetch(url).then(response=>{
-                if(!response.ok){
-                    throw Error('ERROR');
-                }
-                return response.json();
-            }).then(data=>{
-                //console.log(data);
-                this.movieList = data.Search;
-
-            }).catch(error=>{
-                console.log('error getting the data with the api'+error);
-            })
-
-        
+},mounted(){
+     
+   
 }
-    
-},
 
 }
 
